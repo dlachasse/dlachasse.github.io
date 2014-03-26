71dcfc685dc025a7cbf502fa4a1e0dd1ced03ed2
David La Chasse <david.lachasse@gmail.com>
2014-03-25 21:34:28 -0700

    Add scroll animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,10 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 800);
-        return false;
-      }
-    }
-  });
+(function($) {
+  'use strict';
 
-});
+  $('nav a').click(function() {
+    var hash = $(this).attr('href')
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    });
+  });
+}(jQuery));
